584d8d58e7b8ab15c216407851ae0ef3d9405d62
shukoushuu <github_mail@protonmail.com>
2019-08-07 11:46:19 +0800

    add some annotations

--- vae.py
+++ vae.py
@@ -1,5 +1,5 @@
 import torch
-from torch.autograd import Variable
+# from torch.autograd import Variable
 import numpy as np
 import torch.nn.functional as F
 import torchvision
@@ -8,19 +8,19 @@ import torch.optim as optim
 from torch import nn
 import matplotlib.pyplot as plt
 
-
-class Normal(object):
-    def __init__(self, mu, sigma, log_sigma, v=None, r=None):
-        self.mu = mu
-        self.sigma = sigma  # either stdev diagonal itself, or stdev diagonal from decomposition
-        self.logsigma = log_sigma
-        dim = mu.get_shape()
-        if v is None:
-            v = torch.FloatTensor(*dim)
-        if r is None:
-            r = torch.FloatTensor(*dim)
-        self.v = v
-        self.r = r
+# 后续就没用到
+# class Normal(object):
+#     def __init__(self, mu, sigma, log_sigma, v=None, r=None):
+#         self.mu = mu
+#         self.sigma = sigma  # either stdev diagonal itself, or stdev diagonal from decomposition
+#         self.logsigma = log_sigma
+#         dim = mu.get_shape()
+#         if v is None:
+#             v = torch.FloatTensor(*dim)
+#         if r is None:
+#             r = torch.FloatTensor(*dim)
+#         self.v = v
+#         self.r = r
 
 
 class Encoder(torch.nn.Module):
@@ -46,12 +46,13 @@ class Decoder(torch.nn.Module):
 
 
 class VAE(torch.nn.Module):
-    latent_dim = 8
+    latent_dim = 8  # 隐变量维度
 
     def __init__(self, encoder, decoder):
         super(VAE, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
+        # 编码器到均值和log方差的两个网络, 且默认知道编码器的最后一层维度为100, 隐变量维度为8
         self._enc_mu = torch.nn.Linear(100, 8)
         self._enc_log_sigma = torch.nn.Linear(100, 8)
 
@@ -67,14 +68,16 @@ class VAE(torch.nn.Module):
         self.z_mean = mu
         self.z_sigma = sigma
 
-        return mu + sigma * Variable(std_z, requires_grad=False)  # Reparameterization trick
+        return mu + sigma * std_z  # Reparameterization trick
+        # return mu + sigma * Variable(std_z, requires_grad=False)  # Reparameterization trick
 
     def forward(self, state):
-        h_enc = self.encoder(state)
+        h_enc = self.encoder(state)  # 编码器的输出
         z = self._sample_latent(h_enc)
         return self.decoder(z)
 
 
+# 计算KL散度误差项
 def latent_loss(z_mean, z_stddev):
     mean_sq = z_mean * z_mean
     stddev_sq = z_stddev * z_stddev
@@ -95,8 +98,8 @@ if __name__ == '__main__':
 
     print('Number of samples: ', len(mnist))
 
-    encoder = Encoder(input_dim, 100, 100)
-    decoder = Decoder(8, 100, input_dim)
+    encoder = Encoder(input_dim, 100, 100)  # 隐藏层维度为100, 输出维度为100
+    decoder = Decoder(8, 100, input_dim)  # 隐藏层维度为100
     vae = VAE(encoder, decoder)
 
     criterion = nn.MSELoss()
@@ -106,14 +109,20 @@ if __name__ == '__main__':
     for epoch in range(100):
         for i, data in enumerate(dataloader, 0):
             inputs, classes = data
-            inputs, classes = Variable(inputs.resize_(batch_size, input_dim)), Variable(classes)
+            inputs.resize_(batch_size, input_dim)
             optimizer.zero_grad()
-            dec = vae(inputs)
+            dec = vae(inputs)  # 获得的输出端解码
             ll = latent_loss(vae.z_mean, vae.z_sigma)
-            loss = criterion(dec, inputs) + ll
+            loss = criterion(dec, inputs) + ll  # 重建误差 + KL散度误差
             loss.backward()
             optimizer.step()
-            l = loss.data[0]
+            l = loss.item()
+            # l = loss.data[0]
+            """
+            考虑在 PyTorch0.4.0 版本之前广泛使用的 total_loss + = loss.data [0] 模式
+            Loss 是一个包含张量(1, )的Variable, 但是在新发布的 0.4.0 版本中. loss 是一个0维标量, 对于标量的索引是没有意义的
+            使用 loss.item（）从标量中获取 Python 数字
+            """
         print(epoch, l)
 
     plt.imshow(vae(inputs).data[0].numpy().reshape(28, 28), cmap='gray')
